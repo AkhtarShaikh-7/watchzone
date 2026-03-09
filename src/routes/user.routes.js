ce24961bd9e8a7e41aca33713e2def84af9c6711
Akhtar shaikh <mdak3335@gmail.com>
2026-03-09 08:19:57 +0530

    bug fixes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,22 @@
 import {Router} from 'express'
 import { registerUser } from '../controllers/user.controller.js';
+import { upload } from '../middlewares/multer.middleware.js';
 
 const router = Router();
 
-router.route("/register").post(registerUser);
-// router.route("/login").post(loginUSer);
+router.route("/register").post(
+    // this is a middleware : registeruser controller jaane se pahle multer se milnea ??
+    upload.fields([
+        {
+            name:"avatar",
+            maxCount:1,
+        },{
+            name:"coverImage",
+            maxCount:1,
+        }
+    ]),
+    // ye controller logic user details wala
+    registerUser
+);
 
 export default router;
