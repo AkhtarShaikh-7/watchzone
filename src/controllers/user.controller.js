822681fac234f36dc59390b630a4a0a09b7bb4f4
Akhtar shaikh <mdak3335@gmail.com>
2026-03-11 08:41:41 +0530

    add user account management endpoints

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -255,8 +255,95 @@ const refereshAccessToken = asyncHandler(async (req, res) => {
             )
 
     } catch (error) {
-        throw new ApiError(401,error?.message || "Invalid refresh token");
+        throw new ApiError(401, error?.message || "Invalid refresh token");
     }
 })
 
-export { registerUser, loginUser, logoutUser, refreshToken };
+
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+    const { oldPassword, newPassword } = req.body;
+
+    const user = await User.findById(req.body?._id)
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
+
+    if (!isPasswordCorrect) {
+        throw new ApiError(400, "Invalid old password");
+    }
+    user.password = newPassword;
+    await user.save({ validateBeforeSave: false });
+
+    return res.status(200)
+        .json(new ApiResponse(200, {}, "Password changed successfully"))
+})
+
+const getCurrentUser = asyncHandler(async (req, res) => {
+    return res.status(200)
+        .json(200, req.user, "current user fetched successfully")
+})
+
+
+const updateAccountDetails = asyncHandler(async (req, res) => {
+    const { fullName, email } = req.body;
+
+    if (!(fullName || email)) {
+        throw new ApiError(400, "All fields are required");
+    }
+
+    const user = User.findByIdAndUpdate(
+        // ye req.id middleware se arha hai shayd qki wo return kr rh tha if usr login hai tu
+        req.user?._id,
+        {
+            $set: {
+                fullName,
+                email: email
+            }
+        },
+        { new: true }
+    ).select("-password");
+
+    return res.status(200)
+        .json(new ApiResponse(200, user, "Account details updated successfully"));
+})
+
+
+const updateUserAvatar = asyncHandler(async (req, res) => {
+    // file kaise lena hai
+    const avatarLocalPath = req.file?.path
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar file is missing");
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+
+    if (!avatar.url) {
+        throw new ApiError(400, "Error while uploading on avatar")
+    }
+
+    // db me save krna hai use kro ye method findByIdAndUpdate nad kiska krna hai ye req.user?._id ye id ka
+    // kya set krn  hai avatar kisese set krma hai avatar.url  abb return krdo user ko ye new:true   bss password mat send kro select("-password")
+   const user=  await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                avatar: avatar.url
+            }
+        }, { new: true }
+    ).select("-password"); 
+
+    return res.status(200)
+    .json(new ApiResponse(200,user,"avatar image updated successfully"))
+})
+
+
+export {
+    registerUser,
+    loginUser,
+    logoutUser,
+    refreshToken,
+    refereshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar
+};
